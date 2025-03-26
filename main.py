0e739e66d7c177ecea6cad8f7f49d4bacb95cde7
trilochanp3 <pradhantrilochan901@gmail.com>
2025-03-26 22:48:18 +0530

    dvc added

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from src.cnnClassifier import logger
+from src.cnnClassifier import lo
 from src.cnnClassifier.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 from src.cnnClassifier.pipeline.stage_02_prepare_base_model import PrepareBaseModelTrainingPipeline
 from src.cnnClassifier.pipeline.stage_03_model_training import ModelTrainingPipeline
